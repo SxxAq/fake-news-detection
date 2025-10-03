317e4f96f73f7da8030e71fbd7fd085dcc269046
aqueel192@gmail.com <aqueel192@gmail.com>
2025-10-03 23:56:29 +0530

    model training and evaluatiohn

--- main.py
+++ main.py
@@ -1,48 +1,55 @@
 import pandas as pd
 from src.preprocessing import preprocess_text
-from src.features import get_bow_features,get_tfidf_features
+from src.features import get_bow_features, get_tfidf_features
+from src.model import train_model, evaluate_models
 
 
 def load_data():
-  df_fake=pd.read_csv("data/Fake.csv")
-  df_true=pd.read_csv("data/True.csv")
+    df_fake = pd.read_csv("data/Fake.csv")
+    df_true = pd.read_csv("data/True.csv")
 
-  # labeling fake/real news
-  df_fake["label"]=0
-  df_true["label"]=1
+    # labeling fake/real news
+    df_fake["label"] = 0
+    df_true["label"] = 1
 
-  #combining the datasets, shuffling them randomly, and resetting the index
-  # to prepare a clean, randomized dataset for training a machine learning model.
-  df=pd.concat([df_fake,df_true]).sample(frac=1).reset_index(drop=True)
-  return df
+    # combining the datasets, shuffling them randomly, and resetting the index
+    # to prepare a clean, randomized dataset for training a machine learning model.
+    df = pd.concat([df_fake, df_true]).sample(frac=1).reset_index(drop=True)
+    return df
 
 
 def preprocess_dataset(df):
-  df['clean_text_final'] = df['text'].apply(preprocess_text)
-  return df
+    df["clean_text_final"] = df["text"].apply(preprocess_text)
+    return df
 
 
 def main():
-  # Load dataset
-  df = load_data()
-  print("Dataset info:")
-  print(df.info())
-  print("Label distribution:")
-  print(df['label'].value_counts())
+    # Load dataset
+    df = load_data()
+    print("Dataset info:")
+    print(df.info())
+    print("Label distribution:")
+    print(df["label"].value_counts())
 
-  # Preprocess
-  df = preprocess_dataset(df)
-  print("Sample preprocessed text:")
-  print(df[['text', 'clean_text_final']].head())
-  
-  # Feature Engineering
-  X_tfidf,tfidf_vectorizer=get_tfidf_features(df['clean_text_final'])
-  print("TF-IDF Feature Matrix Shape: ",X_tfidf.shape)
+    # Preprocess
+    df = preprocess_dataset(df)
+    print("Sample preprocessed text:")
+    print(df[["text", "clean_text_final"]].head())
 
-  X_bow,bow_vectorizer=get_tfidf_features(df["clean_text_final"])
-  print("Bag of words feature matrix shape: ",X_bow.shape)
-  
+    # Feature Engineering
+    X_tfidf, tfidf_vectorizer = get_tfidf_features(df["clean_text_final"])
+    print("TF-IDF Feature Matrix Shape: ", X_tfidf.shape)
+
+    X_bow, bow_vectorizer = get_tfidf_features(df["clean_text_final"])
+    print("Bag of words feature matrix shape: ", X_bow.shape)
+
+    # Model training
+    y = df["label"]
+
+    X = X_tfidf
+    models, X_train, X_test, y_train, y_test = train_model(X, y)
+    evaluate_models(models, X_test, y_test)
 
 
-if __name__=="__main__":
-  main()
+if __name__ == "__main__":
+    main()
---
+++ src/model.py
@@ -0,0 +1,39 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+
+def train_model(X, y, test_size=0.2, random_state=42):
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=random_state
+    )
+    models = {}
+
+    # logistic regression
+    lr = LogisticRegression(max_iter=1000)
+    lr.fit(X_train, y_train)
+    models["LogisticRegression"] = lr
+
+    # Naive Bayes
+    nb = MultinomialNB()
+    nb.fit(X_train, y_train)
+    models["NaiveBayes"] = nb
+
+    # random forest
+    rf = RandomForestClassifier(n_estimators=100, random_state=random_state)
+    rf.fit(X_train, y_train)
+    models["RandomForest"] = rf
+
+    return models, X_train, y_train, X_test, y_test
+
+
+def evaluate_models(models, X_test, y_test):
+    for name, model in models.items():
+        y_pred = model.predict(X_test)
+        print(f"======{name}======")
+        print("Accuracy: ", accuracy_score(y_test, y_pred))
+        print("Classification Report:\n", classification_report(y_test, y_pred))
+        print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+        print("\n")
